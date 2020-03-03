7e4251294793678c090f41ab51f935ca3f6ebbfc
krzysztofic <krzysztof.iciek@gmail.com>
2020-03-03 23:58:03 +0100

    pagination fixed, refactor old solutions in css

--- strona/urls.py
+++ strona/urls.py
@@ -8,7 +8,6 @@ app_name = 'strona'
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^$', PostView.as_view(), name='post_View'),
-    # url(r'^$', PostView.as_view(), name='post_View'),
     url(r'^(?P<year>\d{4})/(?P<month>\d+)/$',PostMonthArchiveView.as_view(), name='month_archive'),
     url(r'^(?P<slug>[-\w]+)/$', PostDetailsView.as_view(), name='post_details'),
 ]
